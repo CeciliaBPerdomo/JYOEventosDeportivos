4b97a0674085ec7863909f1f74040bd46c74be96
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2024-12-14 19:48:00 -0300

    Proximas carreras

--- src/app/components/iconos/proximasIconos.js
+++ src/app/components/iconos/proximasIconos.js
@@ -1,7 +1,38 @@
+import Image from "next/image"
+
+// CSS
+import "../../css_components/proximasIconos.css"
+
 const ProximasIconos = () => {
     return (
-        <div>
-            <p>Iconos</p>
+        <div className="mb-2">
+            <div className="flex justify-center items-center space-x-4 mt-4">
+                <Image
+                    src="/images/proximas/icono_nadar.jpg"
+                    alt="card-image"
+                    width={100}
+                    height={65}
+                />
+
+                <Image
+                    src="/images/proximas/Icono_bici.jpg"
+                    alt="card-image"
+                    width={100}
+                    height={65}
+                />
+
+                <Image
+                    src="/images/proximas/Icono_correr.jpg"
+                    alt="card-image"
+                    width={100}
+                    height={65}
+                />
+            </div>
+            <div className="flex justify-center items-center space-x-4 mt-1 text-center">
+                <p className="proximas_iconos_p">1,9km Natación</p>
+                <p className="proximas_iconos_p">90km Ciclismo</p>
+                <p className="proximas_iconos_p">21km Pedestrismo</p>
+            </div>
         </div>
     )
 }
--- src/app/components/proximas.js
+++ src/app/components/proximas.js
@@ -10,11 +10,15 @@ const ProximasCarreras = () => {
     return (
         <div className="px-4 md:px-8 lg:px-16 pt-5"
             style={{ backgroundColor: "#F9F9F9" }}>
-            <h1 className="proximas_titulo text-start text-2xl md:text-3xl font-bold my-6">
+            <h1 className="proximas_titulo 
+            md:text-center md:text-3xl 
+            sm:text-center
+            font-bold my-6">
                 Próximas carreras
             </h1>
             <div className="flex items-center justify-center ">
-                <div className="relative flex flex-col md:flex-row w-full my-6 bg-white shadow-sm border 
+                <div className="relative flex flex-col 
+                md:flex-row  my-6 bg-white shadow-sm border 
                 border-slate-200 rounded-lg">
                     <div className="relative p-2.5 md:w-2/5 shrink-0 overflow-hidden">
                         <Image
@@ -25,26 +29,39 @@ const ProximasCarreras = () => {
                             className="h-full w-full rounded-md md:rounded-lg object-cover"
                         />
                     </div>
-                    <div className="p-6 relative flex flex-col justify-between">
-                        <div>
-                            <button className="absolute top-2 right-2 sm:top-4 sm:right-4 md:top-4 md:right-4 
+                    <div>
+                        <div className="p-6 relative flex flex-col justify-between"
+                        //   style={{width: "800px"}} --> Tamaño pc
+                        >
+                            <div>
+                                <button className="absolute top-2 right-2 sm:top-4 sm:right-4 md:top-4 md:right-4 
                             bg-teal-600 py-1 px-3 sm:px-4 text-[10px] sm:text-xs md:text-sm lg:text-base 
                             text-white rounded-full shadow-sm transition-all proximas_inscripciones_abiertas"
-                            >
-                                Inscripciones abiertas
-                            </button>
-                        </div>
-                        <div>
-                            <h4 className="text-slate-800 text-lg sm:text-xl mt-9 md:text-2xl font-semibold proximas_h4">
-                                Triatlón Half Las Cañas
-                            </h4>
-                            <p className="mb-6 sm:mb-8 proximas_p text-sm sm:text-base">
-                                Las Cañas, Río Negro
-                            </p>
-                        </div>
+                                >
+                                    Inscripciones abiertas
+                                </button>
+                            </div>
+                            <div>
+                                <h4 className="text-slate-800 text-lg sm:text-xl mt-9 md:text-2xl font-semibold proximas_h4">
+                                    Triatlón Half Las Cañas
+                                </h4>
+                                <p className="mb-6 sm:mb-8 proximas_p text-sm sm:text-base">
+                                    Las Cañas, Río Negro
+                                </p>
+                            </div>
 
-                        <div>
-                            <ProximasIconos />
+                            <div>
+                                <ProximasIconos />
+                                <div className="flex flex-col items-center justify-center space-y-2 mt-4">
+                                    <button className="proximas_detalle_carrera">
+                                        Ver detalles de la carrera
+                                    </button>
+
+                                    <button className="proximas_inscribite_carrera mt-1">
+                                        INSCRIBITE !
+                                    </button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
