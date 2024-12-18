470361b520c3e54735dffd4522a9081fe9d36299
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2024-12-18 18:55:49 -0300

    Carreras

--- src/app/carreras/page.js
+++ src/app/carreras/page.js
@@ -1,21 +1,23 @@
-//CSS
+import Half from "../components/proximas/20250427"
+
+// CSS 
 import "../css_components/proximas.css"
 
-import Half from "./proximas/20250427"
-
-const ProximasCarreras = () => {
-    return (
+const Carreras = () => {
+return (
+    <div> 
         <div className="px-4 md:px-8 lg:px-16 pt-5"
             style={{ backgroundColor: "#F9F9F9" }}>
             <h1 className="proximas_titulo 
             md:text-center md:text-3xl 
             sm:text-center
             font-bold my-6">
-                Próximas carreras
+                Carreras
             </h1>
-            <Half />
-        </div>
-    )
+        <Half />
+    </div>
+    </div>
+)
 }
 
-export default ProximasCarreras
+export default Carreras
